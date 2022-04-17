c698d6135e920e8b8c91ecba04043fcee4a5ab38
taijusugahara <96ayutuki@gmail.com>
2022-04-17 17:03:15 +0900

    scroll by time

--- calender/static/js/app.js
+++ calender/static/js/app.js
@@ -2,7 +2,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   var selected_info = ""
   var calendarEl = document.getElementById('calendar');
-  
+  var now = new Date();
+  var h = now.getHours();
+  console.log(h)
   var calendar = new FullCalendar.Calendar(calendarEl, {
 
     headerToolbar: {
@@ -29,12 +31,19 @@ document.addEventListener('DOMContentLoaded', function () {
                 },
         allDayText: '終日',
 
+        // slotMinTime: "12:00:00",
+        scrollTime: "12:00:00",
+        
+
       // dateClick: function() {
       //   // alert('dayClick')
       //   $(".day").removeClass("none")
       // },
 
       // selectLongPressDelay:0,
+      allDayDidMount:function(){
+        console.log("xxxxx")
+      },
       views: {
         dayGridMonth:{
           dayMaxEvents:2,
@@ -43,9 +52,15 @@ document.addEventListener('DOMContentLoaded', function () {
       datesSet:function(date){
         $(".day").addClass('none')
         if(date.view.type == "timeGridDay"){
+          
           console.log(date.view)
           $(".day").html(date.view.currentStart)
           $(".day").removeClass('none')
+          console.log($(".fc-timegrid-slot").eq(18).offset().top)//30分単位
+          var scroll_hour = h * 2
+          var scroll_height = $(".fc-timegrid-slot-label").eq(18).offset().top
+          // var scroll_height = $(".fc-timegrid-slot-label").eq(scroll_hour).offset().top
+          $(window).scrollTop(scroll_height);
         }
       },
       eventClick:function (event) {
@@ -86,12 +101,26 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log(selected_info.end)
     calendar.addEvent({
         title: selected_info.start,
-        start: selected_info.start,
-        end: selected_info.end,
-        allDay: true,
+        start : selected_info.start,
+        end: selected_info.start
+
+        // start: selected_info.start,
+        // end: selected_info.end,
+        // allDay: true,
     });
   })
 
+  function setScrollTime() {
+    console.log(15)
+    // setTimeout(
+      calendar.scrollToTime(200)
+    // )
+    
+    calendar.setOption('scrollTime', '04:00:00')
+    // console.log(calendar.getOption('scrollTime'))
+  }
+
+  
   // $("#button").on("click",function(){
   //             calendar.addEvent({
   //                 title: "dammy",
